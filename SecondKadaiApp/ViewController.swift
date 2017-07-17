9a77fdab267132a2a79ab96cec98a5d214714313
Takao <mizuno@MacBook-Pro.local>
2017-07-17 17:10:48 +0900

    commit

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var textField: UITextField!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,15 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    let resultViewController:ResultViewController = segue.destination as! ResultViewController
+    resultViewController.name = textField.text!
+  }
+  
+  @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    
+  }
 
 
 }
-
